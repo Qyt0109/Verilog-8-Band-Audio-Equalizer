a55e4225b214c85b7d3df97027773927735a86dd
DaoQuyet <nmhtdyt@gmail.com>
2024-04-03 21:51:23 +0700

    no message

--- Modules/counter.v
+++ Modules/counter.v
@@ -4,7 +4,7 @@ module counter (
     input            rst,
     output reg [5:0] current_count
 );
-  always @(posedge clk or posedge rst) begin : p_count
+  always @(posedge clk or posedge rst) begin
     if (rst == 1) begin
       current_count <= 6'b111111;
     end else begin
--- Modules/delay_pipeline_tb.v
+++ Modules/delay_pipeline_tb.v
@@ -1,7 +1,7 @@
 // python compile.py phase_check phase_check.v phase_check_tb.v counter.v
 `timescale 1ns / 1ps
 
-module phase_check_tb;
+module delay_pipeline_tb;
 
   reg        clk = 0;
   reg        clk_enable = 1;
@@ -16,31 +16,45 @@ module phase_check_tb;
       .current_count(current_count)
   );
 
-  wire phase_63, phase_0, control_phase_bar;
+  wire phase_63, phase_0;
 
-  phase_check uut (
+  phase_check phase_check_inst (
       .clk_enable(clk_enable),
       .current_count(current_count),
       .phase_63(phase_63),
+      .phase_0(phase_0)
+  );
+
+  localparam NUMBER_OF_PIPE = 64;
+
+  reg signed  [15:0] i_signal_sample;
+  wire signed [15:0] o_delayed_sample;
+
+  delay_pipeline uut (
+      .clk(clk),
+      .rst(rst),
       .phase_0(phase_0),
-      .control_phase_bar(control_phase_bar)
+      .phase_63(phase_63),
+      .i_signal_sample(i_signal_sample),
+      .o_delayed_sample(o_delayed_sample)
   );
 
   parameter CLK_PERIOD = 10;  // 100MHz clock = 10ns period
   always #((CLK_PERIOD) / 2) clk = ~clk;
 
   initial begin
-    $dumpfile("./Simulate/phase_check.vcd");
+    $dumpfile("./Simulate/delay_pipeline.vcd");
     $dumpvars;
     rst = 1;
+    i_signal_sample = 0;
     #(CLK_PERIOD * 10);
     rst = 0;
-    #(CLK_PERIOD * 100);
-    rst = 1;
-    #(CLK_PERIOD * 6);
-    rst = 0;
-    #(CLK_PERIOD * 200);
+    #(CLK_PERIOD * 20000);
     $finish;
+  end
+
+  always @(posedge phase_0) begin
+    i_signal_sample = $signed($urandom_range(1, 200));
   end
 
 endmodule  //counter_tb
